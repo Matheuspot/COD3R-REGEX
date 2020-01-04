d00984d21f61e3597c3de1582fa6a12bb45db950
Matheus Antonio <matheusantonio.info@gmail.com>
2020-01-03 22:43:44 -0300

    push

--- executandoRegex/flags.js
+++ executandoRegex/flags.js
@@ -1,6 +1,10 @@
-// g - global (reconhece todas os caracteres correspondentes ao regex)
-// i - ignore case (ignora letras maiúsculas e minúsculas)
-// Entre das duas barras "//" fica uma expressão regular 
+/*
+*Versao 1 - Flags
+*g - global (reconhece todas os caracteres correspondentes ao regex)
+*i - ignore case (ignora letras maiúsculas e minúsculas)
+*Entre das duas barras "//" fica uma expressão regular 
+*/
+
 
 const texto = 'Carlos assinou o abaixo-assinado.'
 console.log(texto.match(/C|ab/))
