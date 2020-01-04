6e513fb22cb31c2b8cdea980dbb321840657cbd8
Matheus Antonio <matheusantonio.info@gmail.com>
2020-01-04 14:51:19 -0300

    Aula 9 - Parte 1

---
+++ caracteres/OlaRegex.js
@@ -0,0 +1,11 @@
+/*
+console.log(regex.test(texto))
+console.log(regex.exec(texto))
+*/
+
+
+const texto = 'Casa bonita é a casa amarela da esquina com a Rua ACASALAR. '
+
+const regex = /casa/gi
+console.log(texto.match(regex))
+
