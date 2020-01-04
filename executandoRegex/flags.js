a14729419fe9b7531ddf7acba7538bf544232b61
Matheus <42229063+Matheuspot@users.noreply.github.com>
2020-01-03 22:34:23 -0300

    Add files via upload

---
+++ executandoRegex/flags.js
@@ -0,0 +1,8 @@
+// g - global (reconhece todas os caracteres correspondentes ao regex)
+// i - ignore case (ignora letras maiúsculas e minúsculas)
+// Entre das duas barras "//" fica uma expressão regular 
+
+const texto = 'Carlos assinou o abaixo-assinado.'
+console.log(texto.match(/C|ab/))
+console.log(texto.match(/c|ab/i))
+console.log(texto.match(/ab|c/gi))
